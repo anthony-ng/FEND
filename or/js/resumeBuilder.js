646bcfd7e48bb693ac9077c92ddca4d9ee54a351
Anthony Ng <thomsonng123@gmail.com>
2017-06-26 13:53:24 -0700

    added JS obejcts

--- or/js/resumeBuilder.js
+++ or/js/resumeBuilder.js
@@ -1,3 +1,66 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+
+var bio = {
+    name: 'Anthony',
+    role: 'Software Engineer',
+    contacts: {
+        mobile: '(859)462-0836',
+        email: 'thomsonng123@gmail.com',
+        github: 'https://github.com/anthony-ng',
+        twitter: '',
+        location: 'Palo Alto, CA'
+    },
+    welcomeMessage: 'Welcome Everyone!',
+    skills: ['JavaScript', 'HTML5', 'CSS3'],
+    biopic: '#',
+    display: function() {
+
+    }
+};
+
+var education = {
+    schools: [{
+        name: 'UC Berkeley',
+        location: 'Berkeley, CA',
+        degree: 'Bachelor of Arts',
+        majors: ['Statistics'],
+        dates: 'Aug 2008 - May 2013',
+        url: 'https://berkeley.edu'
+    }],
+    onlineCourses: [{
+        title: 'Front-End Nanodegree',
+        school: 'Udacity',
+        dates: 'Jun 2017 - ',
+        url: 'https://udacity.com'
+    }],
+    display: function() {
+
+    }
+};
+
+var work = {
+    jobs: [{
+        employer: 'MongoDB, Inc.',
+        title: 'Technical Services Tools Engineer',
+        location: 'Palo Alto, CA',
+        dates: 'Feb 2017 - ',
+        description: 'Technical Services Tools'
+    }],
+    display: function() {
+
+    }
+};
+
+var projects = {
+    projects: [{
+        title: 'Online Resume',
+        dates: 'Jun 2017 - ',
+        description: 'an interactive online resume',
+        images: ['#']
+    }],
+    display: function() {
+
+    }
+};
